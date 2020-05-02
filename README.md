bc0d3e7df0b3f3d1d48ded2a45fc16ecd8d44b7e
Kelly <k.tompkins005@gmail.com>
2020-05-01 23:37:01 -0500

    changing data for dictionary from json file to pydictionary

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Dictionary
+
+MVP:
+Uses PyDictionary to find word according to user input
+
+Future:
+Also lists synonyms from thesaurus also from PyDictionary
--- app.py
+++ app.py
@@ -1,28 +1,12 @@
 import json
 from difflib import get_close_matches
+from PyDictionary import PyDictionary
 
-data = json.load(open("data.json", 'r'))
-
+data = PyDictionary()
 
 def translate(w):
     w = w.lower()
-    if w in data:
-        return data[w]
-    elif w.title() in data:
-        return data[w.title()]
-    elif w.upper() in data:
-        return data[w.upper()]
-    elif len(get_close_matches(w, data.keys())) > 0:
-        yn = input("Did you mean %s instead? Enter Y if yes or N if no: " % get_close_matches(w, data.keys())[0])
-        if yn == "Y":
-            return data[get_close_matches(w, data.keys())[0]]
-        elif yn == "N":
-            return "That word doesn't exist. Please double check it."
-        else:
-            return "We didn't understand your entry."
-
-    else:
-        return "That word doesn't exist. Please double check your spelling."
+    return data.meaning(w)
 
 
 w = input("Enter a word: ")
