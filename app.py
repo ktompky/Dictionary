17adb8a03ea546787b65f9bd3f30c3e2881ded8f
Kelly <k.tompkins005@gmail.com>
2020-05-06 11:57:34 -0500

    add try and except error handling in case definition doesn't exist

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
 import json
 from difflib import get_close_matches
-from config import dictionaryKey,thesaurusKey
+from config import dictionaryKey, thesaurusKey
 import requests
 
 w = input("Hello, what would you like to know the definition of? ")
 
+
 def translate(w):
-   response = requests.get(f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{w}?key={dictionaryKey}")
-   definition = response.json()
-   new_def = definition[0]['def'][0]
-   
-   for key,value in new_def.items():
-       print(key)
-       for item in value:
-           print(item[0][1]['dt'][0][1])
-       
+    response = requests.get(
+        f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{w}?key={dictionaryKey}")
+    definition = response.json()
+    new_def = definition[0]['def'][0]
+
+    for key, value in new_def.items():
+        print(key)
+        i = 1
+        for item in value:
+           
+            try:
+                word = str(i) + "." + item[0][1]['dt'][0][1]
+                i += 1
+                new_w = word.replace('{bc}', ' ')
+                print(new_w)
+            except:
+                continue
+
 
 if __name__ == '__main__':
     translate(w)
